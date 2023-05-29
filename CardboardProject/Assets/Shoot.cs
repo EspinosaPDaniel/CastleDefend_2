eca9062fab3364ecb4aebcb2578e4d3a001d3380
EspinosaPDaniel <115702791+EspinosaPDaniel@users.noreply.github.com>
2023-05-29 12:33:03 +0200

    commit

--- CardboardProject/Assets/Shoot.cs
+++ CardboardProject/Assets/Shoot.cs
@@ -18,29 +18,28 @@ public class Shoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (Input.touchCount > 0)
-        //{
-           // Touch touch = Input.GetTouch(0);
-           // if (touch.phase == TouchPhase.Began)
-           // {
-               // if(Time.time>shotRateTime){
-               // GameObject newArrow;
-               // newArrow=Instantiate(arrow, spawnPoint.position,Quaternion.Euler(spawnPoint.rotation.x,spawnPoint.rotation.y+90f,spawnPoint.rotation.z));
-               // newArrow.GetComponent<Rigidbody>().AddForce(spawnPoint.forward*shotForce);
-               // shotRateTime=Time.time+shotRate;
-              //  Destroy(newArrow,2);
-            //}
-            //}
-        //}
-    
-        if(Input.GetKeyDown(KeyCode.F)){
-            if(Time.time>shotRateTime){
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                if(Time.time>shotRateTime){
                 GameObject newArrow;
                 newArrow=Instantiate(arrow, spawnPoint.position,Quaternion.Euler(spawnPoint.rotation.x,spawnPoint.rotation.y+90f,spawnPoint.rotation.z));
                 newArrow.GetComponent<Rigidbody>().AddForce(spawnPoint.forward*shotForce);
                 shotRateTime=Time.time+shotRate;
                 Destroy(newArrow,2);
             }
+            }
         }
+    
+        //if(Input.GetKeyDown(KeyCode.F)){
+          //  if(Time.time>shotRateTime){
+            //    GameObject newArrow;
+              //newArrow.GetComponent<Rigidbody>().AddForce(spawnPoint.forward*shotForce);
+               // shotRateTime=Time.time+shotRate;
+               // Destroy(newArrow,2);
+            //}
+        //}
     }
 }
